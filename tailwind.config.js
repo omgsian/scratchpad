68af240626d3e2972859f23210772cbb41800da1
Ridzwan <4947140+omgsian@users.noreply.github.com>
2023-07-21 15:57:37 +0800

    configured tailwind, added output.css to main_page.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./templates/**/*.html", "./static/src/**/*.js"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
