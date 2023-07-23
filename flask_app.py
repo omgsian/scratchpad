408fb59b24a380d34593af1b48c0db2ab50e51b3
Ridzwan <4947140+omgsian@users.noreply.github.com>
2023-07-23 10:44:16 +0800

    fixed css

--- flask_app.py
+++ flask_app.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
-comments = []
+comments = ['hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', ]
 
 
 @app.route("/", methods=["GET", "POST"])
