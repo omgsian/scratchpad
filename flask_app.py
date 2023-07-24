ce8ee9e150defa11c3002f2006fbed4f15701420
Ridzwan <4947140+omgsian@users.noreply.github.com>
2023-07-25 00:48:00 +0800

    edited the comments

--- flask_app.py
+++ flask_app.py
@@ -27,15 +27,14 @@ class Comment(db.Model):
     content = db.Column(db.String(4096))
 
 
-comments = ["Hello 👋🏼"]
-
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("main_page.html", comments=comments)
+        return render_template("main_page.html", comments=Comment.query.all())
 
-    comments.append(request.form["contents"])
+    comment = Comment(content=request.form["contents"])
+    db.session.add(comment)
+    db.session.commit()
     return redirect(url_for("index"))
 
 
