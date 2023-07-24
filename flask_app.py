9fa6f84a8575d19d9e99fdedef0f5eb3dde6b06d
Ridzwan <4947140+omgsian@users.noreply.github.com>
2023-07-25 00:36:00 +0800

    added db dependencies

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,27 @@
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+SQLALCHEMY_DATABASE_URI = (
+    "mysql+mysqlconnector://omgwhut:{password}@{hostname}/{databasename}".format(
+        username="omgwhut",
+        password="mrh1990x",
+        hostname="omgwhut.mysql.pythonanywhere-services.com",
+        databasename="omgwhut$comments",
+    )
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
 
 # comments = ["hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello2", ]
 
 comments = ["Hello 👋🏼"]
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -13,6 +30,7 @@ def index():
 
     comments.append(request.form["contents"])
     return redirect(url_for("index"))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
