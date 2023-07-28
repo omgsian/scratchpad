9814d3574d7b85a5aca261b1acc249eea6ad81ba
Ridz <hulloridzwan+github@gmail.com>
2023-07-28 09:01:52 +0000

    login logic

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import login_user, LoginManager, UserMixin
+from flask_login import login_user, LoginManager, UserMixin, login_required
 from werkzeug.security import check_password_hash, generate_password_hash
 
 
@@ -85,6 +85,11 @@ def login():
     login_user(user)
     return redirect(url_for('index'))
 
+@app.route("/logout/")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
